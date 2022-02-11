5e7833424695482e9b5c1164e7402b7d79f81c5f
Min <limminxuan14@gmail.com>
2022-02-11 18:33:25 +0800

    services: encryption & captcha setup

---
+++ sitconnect/Services/Captcha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace sitconnect.Services
+{
+    public class Captcha
+    {
+        public static bool ReCaptchaPassed (string gRecaptchaResponse, string siteKey)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var res = httpClient.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret={siteKey}&response={gRecaptchaResponse}").Result;
+            if (res.StatusCode != HttpStatusCode.OK) 
+            {
+                return false;
+            }
+            string JSONres = res.Content.ReadAsStringAsync().Result;
+            dynamic JSONdata = JObject.Parse(JSONres);
+
+            if (JSONdata.success != "true" || JSONdata.score <= 0.5m)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
