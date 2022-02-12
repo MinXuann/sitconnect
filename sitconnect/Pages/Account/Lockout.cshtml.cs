b41b9bd6da23abe605ec792c2defabb52ad6d415
Min <limminxuan14@gmail.com>
2022-02-12 15:20:22 +0800

    templates: account lockout

---
+++ sitconnect/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sitconnect.Pages.Account
+{
+    public class Lockout : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
